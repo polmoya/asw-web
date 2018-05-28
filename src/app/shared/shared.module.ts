9e0c4223ed2404a3ec75ba1805fe850b6e05b95a
Sergio <sergio.paredes@est.fib.upc.edu>
2018-05-28 09:39:55 +0200

    Move Angular Material dependencies to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,28 @@ import {NgModule} from '@angular/core';
 
 import {HttpService} from './http.service';
 import {HttpClientModule} from '@angular/common/http';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatCardModule, MatInputModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
-  imports: [HttpClientModule],
-  exports: [],
+  imports: [
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatCardModule
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatCardModule
+  ],
   providers: [HttpService]
 })
 export class SharedModule {
