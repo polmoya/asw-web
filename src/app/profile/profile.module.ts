fb07615fd7752ec2c65637e6269c6312f7b38797
Joan <jsanchezgarcia13@gmail.com>
2018-05-30 16:10:48 +0200

    comments button

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,10 +3,11 @@ import {NgModule} from '@angular/core';
 import {MaterialModule} from '../material.module';
 import {SharedModule} from '../shared/shared.module';
 import {SeeProfileComponent} from './see-profile.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [SeeProfileComponent],
-  imports: [MaterialModule, SharedModule],
+  imports: [MaterialModule, SharedModule, RouterModule],
   exports: []
 })
 export class ProfileModule {
