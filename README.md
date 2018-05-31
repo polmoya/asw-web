6d35f4c011f69a860dbaf27a3a3d8442cf135901
Sergio <sergio.paredes@est.fib.upc.edu>
2018-05-31 22:13:25 +0200

    Use env variables for API endpoint

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+If you want to use a local API deploy it on port 5000 and run `ng serve -c local`.
 
 ## Code scaffolding
 
--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -1,29 +1,30 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
- const API_ENDPOINT = 'http://myhackernews.tk/api/';
-// const API_ENDPOINT = 'http://localhost:5000/api/';
 
 @Injectable()
 export class HttpService {
+
+  private readonly API_URL = environment.apiUrl;
 
   constructor(private httpClient: HttpClient) {
   }
 
   get(url: string): Promise<any> {
-    return this.httpClient.get(API_ENDPOINT + url).toPromise();
+    return this.httpClient.get(this.API_URL + url).toPromise();
   }
 
   post(url: string, params: any): Promise<any> {
-    return this.httpClient.post(API_ENDPOINT + url, params).toPromise();
+    return this.httpClient.post(this.API_URL + url, params).toPromise();
   }
 
   put(url: string, params: any): Promise<any> {
-    return this.httpClient.put(API_ENDPOINT + url, params).toPromise();
+    return this.httpClient.put(this.API_URL + url, params).toPromise();
   }
 
   delete(url: string): Promise<any> {
-    return this.httpClient.delete(API_ENDPOINT + url).toPromise();
+    return this.httpClient.delete(this.API_URL + url).toPromise();
   }
 }
 
