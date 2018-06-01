bb4252b6d55c3d54408d480f1592c88cfcf1a9cd
Sergio <sergio.paredes@est.fib.upc.edu>
2018-06-01 20:55:55 +0200

    Add JWT interceptor. Navigate from See Profile view to My Profile view when requested user is logged user

---
+++ src/app/auth/jwt.interceptor.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import {AuthService} from './auth.service';
+import {ErrorObservable} from 'rxjs-compat/observable/ErrorObservable';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+  constructor(private authenticationService: AuthService,              private router: Router) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authenticationService.getToken();
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: token
+        }
+      });
+    }
+
+    return next.handle(request).catch(err => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
+          this.authenticationService.logout();
+          this.router.navigate(['']);
+        } else {
+          return ErrorObservable.create('error');
+        }
+      }
+    });
+  }
+}
