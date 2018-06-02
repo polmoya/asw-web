18d95261dc36e5a32c11d2a00ef6203d259ed1d9
Joan <jsanchezgarcia13@gmail.com>
2018-06-02 20:18:07 +0200

    my profile design refactor

--- src/app/profile/my-profile.component.ts
+++ src/app/profile/my-profile.component.ts
@@ -7,7 +7,8 @@ import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-my-profile',
-  templateUrl: './my-profile.component.html'
+  templateUrl: './my-profile.component.html',
+  styleUrls: ['./my-profile.component.css']
 })
 export class MyProfileComponent implements OnInit {
 
