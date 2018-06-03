efcc863de287333515a5be9dbb35304fba7965b7
Pol Moya Betriu <pol.moya.betriu@est.fib.upc.edu>
2018-06-03 18:49:01 +0200

    Add new Reply

--- src/app/reply/reply.component.ts
+++ src/app/reply/reply.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Comment} from '../comment/comment.model';
 import {HttpService} from '../shared/http.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {Contribution} from '../contribution/contribution.model';
 
 @Component({
   selector: 'app-reply',
@@ -12,27 +11,18 @@ import {Contribution} from '../contribution/contribution.model';
 
 export class ReplyComponent implements OnInit {
   reply: Comment;
-  parent_comment: Comment;
-  contribution: Contribution;
 
   constructor(private httpService: HttpService, private router: Router, private route: ActivatedRoute) {
     this.reply = new Comment();
-    this.parent_comment = new Comment();
-    this.parent_comment.text = '(Mostrar el títol al comment al que fem el reply)';
-    this.contribution = new Contribution();
   }
 
   ngOnInit() {
-    this.getInfo();
-  }
-
-  async getInfo(): Promise<any> {
-    const id: String = this.route.snapshot.params.id;
-    this.contribution = await this.httpService.get('contributions/' + id);
   }
 
   async doReply(): Promise<any> {
-    await this.httpService.post('asks', this.reply);
+    const parentId: String = this.route.snapshot.params.id;
+    await this.httpService.post('comments/' + parentId + '/replies', this.reply);
     this.router.navigate(['']);
+    this.reply = new Comment();
   }
 }
